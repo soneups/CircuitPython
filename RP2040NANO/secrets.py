d80aefad2bb824daa604d9adc270a1b35ab3a141
Gary Sone <gary.sone@bh.sone.net>
2021-06-19 10:20:19 +0100

    Delete secrets.py
    oops

--- RP2040NANO/secrets.py
+++
@@ -1,8 +0,0 @@
-# This file is where you keep secret settings, passwords, and tokens!
-# If you put them in the code you risk committing that info or sharing it
-
-secrets = {
-    'ssid' : 'BTWholeHome-6TP',
-    'password' : 'HgUhXwtTwT6h',
-    'timezone' : "Europe/London", # http://worldtimeapi.org/timezones
-    }
